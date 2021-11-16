50cf8116b30f21c3df912ecb2982029dd38bd2f1
Vladislav <vywebdev@gmail.com>
2021-11-16 14:22:47 +0200

    wholeamole

--- moles.js
+++ moles.js
@@ -1,5 +1,9 @@
-const show = document.querySelector('#btn');
+
 const moles = document.querySelectorAll('.mole');
+const gameend = document.querySelector('.game_ended_overlay');
+let working = true;
+
+const show = document.querySelector('#btn');
 let opened = false;
 console.log(moles);
 console.log(opened);
@@ -7,7 +11,7 @@ show.addEventListener('click', function() {
     console.log('pressed');
     if (opened) {
         for (let i = 0; i < moles.length; i++) {
-            moles[i].classList.remove('open');
+            //moles[i].classList.remove('open');
             moles[i].classList.remove('fade-in');
             moles[i].classList.add('fade-out');
         }
@@ -16,32 +20,56 @@ show.addEventListener('click', function() {
         for (let i = 0; i < moles.length; i++) {
             moles[i].classList.remove('fade-out');
             moles[i].classList.add('fade-in');
-            moles[i].classList.add('open');
+            //moles[i].classList.add('open');
 
         }
         opened = true;
     }
 });
 
-
 let score = 0;
 document.getElementById('score').innerHTML = score;
 
-
-
-
-
 const reset = document.querySelector('#reset');
 reset.addEventListener('click', () => {
-working = true;
-score = 0;
-time = 60;
-document.getElementById('score').innerHTML = score;
-timer(time);
 
+    working = true;
+    score = 0;
+    time = 10;
+    document.getElementById('score').innerHTML = score;
+    reset.innerHTML = 'Reset Game';
+    
 
+    timer(time);
 
+    let molesMooves = setInterval( () => {
+        let i = Math.trunc(Math.random()*8+1);
+        //console.log(i);
+        if (moles[i].classList.contains('fade-out')){
+        moles[i].classList.remove('fade-out');     // is this class present?
+        }
+        moles[i].classList.add('fade-in');
+        //moles[i].classList.add('open');
+
+        moles[i].addEventListener('click', foo);        // only one click on mole
+
+        function foo() {
+            score++;
+            document.getElementById('score').innerHTML = score;
+            moles[i].removeEventListener('click', foo);
+        }
+
+        let molesinterval = setTimeout (() => {
+            //moles[i].classList.remove('open');
+            moles[i].classList.remove('fade-in');
+            moles[i].classList.add('fade-out');
+        }, 1500)
+        if (!working) {
+            clearInterval(molesMooves);
+        }
+    }, 1000);
 })
+
 
 function timer(time) {
     let myTimer = setInterval(function() {
@@ -52,4 +80,9 @@ function timer(time) {
         clearInterval(myTimer);
       }
     }, 1000);
+}
+
+if (!working) {
+    gameend.classList.add('fade-in');
+    reset.classList.add('fade-in');
 }
